e676557687833f743ac8e7124a25b6821a341fa4
박지호 <devpark29@1thefull.com>
2020-09-10 14:50:45 +0900

    add user admin functions

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -5,10 +5,26 @@ from django.contrib import admin
 
 from users.models import *
 
+
 class UserAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'username', 'email', 'phonenumber', 'is_staff', 'date_joined']
+    search_fields = ['username']
+    ordering = ['-id', 'date_joined']
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'gender', 'date_of_birth', 'height', 'weight']
+    search_fields = ['user']
+    ordering = ['-id']
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user_profile', 'primary', 'province', 'city', 'street_name_address',
+                    'land_lot_number_address']
+    search_fields = ['user_profile']
+    ordering = ['-id']
 
 
 admin.site.register(CustomUser, UserAdmin)
-admin.site.register(UserProfile, UserAdmin)
-admin.site.register(Address, UserAdmin)
+admin.site.register(UserProfile, ProfileAdmin)
+admin.site.register(Address, AddressAdmin)
