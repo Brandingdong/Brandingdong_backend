75a7ed3e538a3e82e6b9756e110458015cc74bc3
박지호 <devpark29@1thefull.com>
2020-09-29 19:47:41 +0900

    fixing

--- apps/carts/models.py
+++ apps/carts/models.py
@@ -1,13 +1,29 @@
-from datetime import datetime
-
 from django.contrib.auth import get_user_model
 from django.db import models
+
+from products.models import Product, ProductOption
 
 User = get_user_model()
 
 
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cart')
-    items = models.ForeignKey('products.Product', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
+class CartItems(models.Model):
+    cart = models.ForeignKey('Cart', blank=True, on_delete=models.CASCADE, verbose_name='카트', related_name='cart_items')
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    option = models.ForeignKey('ProductOption', on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
-    add_time = models.DateTimeField(default=datetime.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    total_price = models.PositiveIntegerField(default=0)
+    delivery_fee = models.PositiveIntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        self.total_price= total
+        super().save(*args, **kwargs)
+        total_price =
+
